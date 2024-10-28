8aa30ac8f114f1846874e79e1a091035dd9a6372
GerardoCasten3 <144501374+GerardoCasten3@users.noreply.github.com>
2024-10-28 17:46:05 -0600

    Upload
    Upload diagrams and database.
    The folder of the project was created.

---
+++ Base de Datos/limpieza.sql
@@ -0,0 +1,146 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 100428 (10.4.28-MariaDB)
+ Source Host           : localhost:3306
+ Source Schema         : limpieza
+
+ Target Server Type    : MySQL
+ Target Server Version : 100428 (10.4.28-MariaDB)
+ File Encoding         : 65001
+
+ Date: 28/10/2024 17:40:35
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for actividades_colonias
+-- ----------------------------
+DROP TABLE IF EXISTS `actividades_colonias`;
+CREATE TABLE `actividades_colonias`  (
+  `id_actividad` int NOT NULL,
+  `id_colonia` int NOT NULL,
+  PRIMARY KEY (`id_actividad`, `id_colonia`) USING BTREE,
+  INDEX `id_colonia`(`id_colonia` ASC) USING BTREE,
+  CONSTRAINT `actividades_colonias_ibfk_1` FOREIGN KEY (`id_actividad`) REFERENCES `actividades_limpieza` (`id_actividad`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `actividades_colonias_ibfk_2` FOREIGN KEY (`id_colonia`) REFERENCES `colonias` (`id_colonia`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of actividades_colonias
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for actividades_limpieza
+-- ----------------------------
+DROP TABLE IF EXISTS `actividades_limpieza`;
+CREATE TABLE `actividades_limpieza`  (
+  `id_actividad` int NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `fecha` date NOT NULL,
+  `retroalimentacion` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `imagenEvidencia` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `id_cuadrilla` int NOT NULL,
+  PRIMARY KEY (`id_actividad`) USING BTREE,
+  INDEX `id_cuadrilla`(`id_cuadrilla` ASC) USING BTREE,
+  CONSTRAINT `actividades_limpieza_ibfk_1` FOREIGN KEY (`id_cuadrilla`) REFERENCES `cuadrillas` (`id_cuadrilla`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of actividades_limpieza
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for colonias
+-- ----------------------------
+DROP TABLE IF EXISTS `colonias`;
+CREATE TABLE `colonias`  (
+  `id_colonia` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `codigo_postal` int NOT NULL,
+  `tipo_asentamiento` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id_colonia`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of colonias
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for cuadrillas
+-- ----------------------------
+DROP TABLE IF EXISTS `cuadrillas`;
+CREATE TABLE `cuadrillas`  (
+  `id_cuadrilla` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id_cuadrilla`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of cuadrillas
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for empleados
+-- ----------------------------
+DROP TABLE IF EXISTS `empleados`;
+CREATE TABLE `empleados`  (
+  `id_empleado` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `cargo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `es_jefe_cuadrilla` tinyint(1) NULL DEFAULT 0,
+  `id_cuadrilla` int NULL DEFAULT NULL,
+  `id_usuario` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id_empleado`) USING BTREE,
+  INDEX `id_cuadrilla`(`id_cuadrilla` ASC) USING BTREE,
+  INDEX `id_usuario`(`id_usuario` ASC) USING BTREE,
+  CONSTRAINT `empleados_ibfk_1` FOREIGN KEY (`id_cuadrilla`) REFERENCES `cuadrillas` (`id_cuadrilla`) ON DELETE SET NULL ON UPDATE RESTRICT,
+  CONSTRAINT `empleados_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of empleados
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for observadores
+-- ----------------------------
+DROP TABLE IF EXISTS `observadores`;
+CREATE TABLE `observadores`  (
+  `id_observador` int NOT NULL AUTO_INCREMENT,
+  `id_usuario` int NULL DEFAULT NULL,
+  `id_actividad` int NULL DEFAULT NULL,
+  `mensaje` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_observador`) USING BTREE,
+  INDEX `id_usuario`(`id_usuario` ASC) USING BTREE,
+  INDEX `id_actividad`(`id_actividad` ASC) USING BTREE,
+  CONSTRAINT `observadores_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `observadores_ibfk_2` FOREIGN KEY (`id_actividad`) REFERENCES `actividades_limpieza` (`id_actividad`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of observadores
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for usuarios
+-- ----------------------------
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios`  (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `rol` enum('ADMINISTRADOR','EMPLEADO') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id_usuario`) USING BTREE,
+  UNIQUE INDEX `username`(`username` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of usuarios
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
